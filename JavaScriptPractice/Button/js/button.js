021a16a50d051e14c4a2a932635386c20618a7c0
adnansadar <adnansadar11@gmail.com>
2020-05-30 07:46:38 +0530

    few changes

--- JavaScriptPractice/Button/js/button.js
+++
@@ -1,9 +0,0 @@
-var btn = document.createElement("Button");//Creating a button
-
-btn.innerHTML = "0";//Button displays 0 initially
-btn.id = "btn";//Give it an id
-document.body.appendChild(btn);
-
-btn.onclick = function() {
-    btn.innerHTML++;//Displays no of clicks on the button
-}
