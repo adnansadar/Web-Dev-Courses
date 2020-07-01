ed10a563bd3f2524287c3f57fc620e790b89d19f
adnansadar <adnansadar11@gmail.com>
2020-07-01 19:46:36 +0530

    changes

--- ReactJS/counter-app/src/components/counters.jsx
+++ ReactJS/counter-app/src/components/counters.jsx
@@ -10,12 +10,46 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
+
+  handleIncrement = (counter) => {
+    console.log(counter);
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    // console.log("Event Handler Called!", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId); //filter out item to be deleted
+    this.setState({ counters });
+    //setState() enqueues changes to the component state and tells React that this component and its children need to be re-rendered with the updated state. This is the primary method you use to update the user interface in response to event handlers and server responses.
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {/* props  */}
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            // value={counter.value}
+            // selected
+            // id={counter.id}//pass id as a prop as key cannot be accessed by it
+            counter={counter} //encapsulation, counter object is carrying all the data of counter
+          >
             <h4>Title</h4>
           </Counter>
         ))}
