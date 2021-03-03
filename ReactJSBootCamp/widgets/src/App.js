1e55bedb18c631ce01b4d0f261251dc0e1e0aa75
adnansadar <adnansadar11@gmail.com>
2021-03-03 22:19:59 +0530

    added dropdown functionality

--- ReactJSBootCamp/widgets/src/App.js
+++ ReactJSBootCamp/widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   { title: "What is react?", content: "Front-End Framework" },
@@ -8,11 +9,31 @@ const items = [
   { title: "How do you use react?", content: "Creating Components" },
 ];
 
+const options = [
+  {
+    label: "A Shade of Red",
+    value: "red",
+  },
+  {
+    label: "A Shade of Green",
+    value: "greed",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [selected, setselected] = useState(options[0]);
   return (
     <div>
-      {/* <h1>Widgets App</h1> */}
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setselected}
+        options={options}
+      />
     </div>
   );
 };
