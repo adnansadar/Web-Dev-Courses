80300f74a7a7c9f926c6e4330b1b830063553748
adnansadar <adnansadar11@gmail.com>
2021-03-05 10:54:01 +0530

    translation functionality added

--- ReactJSBootCamp/widgets/src/App.js
+++ ReactJSBootCamp/widgets/src/App.js
@@ -1,39 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 // import Accordion from "./components/Accordion";
 // import Search from "./components/Search";
-import Dropdown from "./components/Dropdown";
-
-const items = [
-  { title: "What is react?", content: "Front-End Framework" },
-  { title: "Why use react?", content: "Popular" },
-  { title: "How do you use react?", content: "Creating Components" },
-];
-
-const options = [
-  {
-    label: "A Shade of Red",
-    value: "red",
-  },
-  {
-    label: "A Shade of Green",
-    value: "greed",
-  },
-  {
-    label: "A Shade of Blue",
-    value: "blue",
-  },
-];
+// import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-  const [selected, setselected] = useState(options[0]);
   return (
     <div>
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setselected}
-        options={options}
-      />
+      <Translate />
     </div>
   );
 };
--- ReactJSBootCamp/widgets/src/components/Dropdown.js
+++ ReactJSBootCamp/widgets/src/components/Dropdown.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ options, selected, onSelectedChange, label }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const handleBodyClick = (e) => {
+      // if the user clicked on any child element inside the form(ref) then return without modifying the state of open(remains false).
+      if (ref.current && ref.current.contains(e.target)) {
+        return;
+      }
+      // else if the user clicks anywhere else in the body
+      setOpen(false);
+    };
+    document.body.addEventListener("click", handleBodyClick);
+    return () => {
+      document.body.removeEventListener("click", handleBodyClick);
+    };
+  }, []);
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) return null;
     return (
       <div
         key={option.value}
         className="item"
-        onClick={() => onSelectedChange(option)}
+        onClick={(e) => onSelectedChange(option)}
       >
         {option.label}
       </div>
@@ -16,9 +32,9 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a Color</label>
+        <label className="label">{label}</label>
         <div
           onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? "visible true" : ""}`}
